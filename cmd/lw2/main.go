9a7d1dbd1ccc11b09f8082b3b71832c182539ec3
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-04-29 03:58:40 +0300

    Finished client

--- cmd/lw2/main.go
+++ cmd/lw2/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/xXxRisingTidexXx/wt-labs/pkg/jsonrpc"
+	"net/http"
+	"time"
+)
 
+func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetReportCaller(true)
+	client := jsonrpc.NewClient(&http.Client{Timeout: 5 * time.Second}, "http://localhost:8484")
+	log.Info(
+		client.Call(jsonrpc.NewRequest("Pizda", jsonrpc.NewEmpty(), jsonrpc.NewInt(181023))),
+	)
+	log.Info(
+		client.Call(jsonrpc.NewRequest("Pizda", jsonrpc.NewEmpty(), jsonrpc.NewNotification())),
+	)
+	log.Info(
+		client.Call(jsonrpc.NewRequest("Greet", jsonrpc.NewEmpty(), jsonrpc.NewString("abfwefds"))),
+	)
 }

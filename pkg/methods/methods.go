adcaabc771bdf29be3b763f713ffe92f56e966ec
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-05-07 00:39:03 +0300

    Fuck the sleep

--- pkg/methods/methods.go
+++ pkg/methods/methods.go
@@ -2,6 +2,7 @@ package methods
 
 import (
 	"github.com/xXxRisingTidexXx/wt-labs/pkg/jsonrpc"
+	"os"
 )
 
 var Greet = jsonrpc.MethodFunc(
@@ -20,5 +21,25 @@ var Square = jsonrpc.MethodFunc(
 			return jsonrpc.WithError(jsonrpc.NewInvalidParams("Param number must equal 1"))
 		}
 		return jsonrpc.WithResult(params[0] * params[0])
+	},
+)
+
+var StoreIP = jsonrpc.MethodFunc(
+	func(request jsonrpc.Request) jsonrpc.Response {
+		var params []string
+		if e := request.UnmarshalParams(&params); e != nil {
+			return jsonrpc.WithError(e)
+		}
+		if len(params) != 1 {
+			return jsonrpc.WithError(jsonrpc.NewInvalidParams("Param number must equal 1"))
+		}
+		file, err := os.OpenFile("registry.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return jsonrpc.WithError(jsonrpc.NewStructuredError(1000, "Append failed", err))
+		}
+		if _, err := file.WriteString(params[0] + "\n"); err != nil {
+			return jsonrpc.WithError(jsonrpc.NewStructuredError())
+		}
+		return jsonrpc.WithResult("OK")
 	},
 )

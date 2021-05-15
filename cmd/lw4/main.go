392b5045936c60cdb2db54bc4f3ed37d404e3613
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-05-16 01:29:47 +0300

    Defined 7 nodes

--- cmd/lw4/main.go
+++ cmd/lw4/main.go
@@ -13,7 +13,7 @@ func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetReportCaller(true)
 	server := &http.Server{
-		Addr: ":9292",
+		Addr: ":80",
 		ReadTimeout:    15 * time.Second,
 		WriteTimeout:   15 * time.Second,
 		MaxHeaderBytes: 1048576,

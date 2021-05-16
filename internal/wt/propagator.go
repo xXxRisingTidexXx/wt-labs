247352633290c0eeb641a030dfcfd67e1deeba2f
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-05-16 11:39:25 +0300

    Added ip parsing

--- internal/wt/propagator.go
+++ internal/wt/propagator.go
@@ -3,18 +3,36 @@ package wt
 import (
 	"database/sql"
 	"github.com/xXxRisingTidexXx/wt-labs/pkg/jsonrpc"
+	"net"
+	"net/http"
+	"time"
 )
 
 type Propagator struct {
 	source  string
 	targets []string
 	db      *sql.DB
+	clients map[string]*jsonrpc.Client
 }
 
 func NewPropagator(source string, targets []string, db *sql.DB) *Propagator {
-	return &Propagator{source, targets, db}
+	clients := make(map[string]*jsonrpc.Client, len(targets))
+	for _, target := range targets {
+		clients[target] = jsonrpc.NewClient(
+			&http.Client{Timeout: 5 * time.Second},
+			"http://wt-app-"+target+"/",
+		)
+	}
+	return &Propagator{source, targets, db, clients}
 }
 
-func (p *Propagator) ServeJSONRPC(_ jsonrpc.Request) jsonrpc.Response {
+func (p *Propagator) ServeJSONRPC(request jsonrpc.Request) jsonrpc.Response {
+	var ips []net.IP
+	if e := request.UnmarshalParams(&ips); e != nil {
+		return jsonrpc.WithError(e)
+	}
+	if len(ips) != 1 {
+		return jsonrpc.WithError(jsonrpc.NewInvalidParams("IP number must equal 1"))
+	}
 	return jsonrpc.WithResult(p.source)
 }
